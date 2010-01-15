fc8badcb5c863ea27d4c90fd6d07cdfdde641adc
Darius <jdarius@jdarius.local>
2010-01-15 12:57:40 -0500

    revert / simplify for git publication

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@ require 'rubygems'
 require 'sinatra'
 require 'builder'
 
+=begin
+  This simple script demonstrates building custom XML for twilio voice-apps. (according to "TwiML" proprietary open specification)
+
+  Possible exercises for the reader:
+    1) pressing any key during a song to return to the main menu
+    2) understanding mp3 caching (i.e., why am I using a 'get' for the 'play' command, rather than a 'post')
+    3) pressing any key during a song to rate the song, and continue playing (requires adding a data storage component. Flatfile?)
+    4) have the system hang up with a polite message
+    5) have the system record a user singing a lyric, then string them together
+    6) separate the intro jingle/instructions from the actual menu that gets repeated.
+    7) change the voice (to 'woman')
+    8) have the system call you back
+    9) get a feed notification of who is calling this number through the twilio regular API 
+    
+  Background info and a similar Rails script available at:
+  http://dariusroberts.com/posts/dial-a-song-sinatra-twilio-to-reproduce-tmbg-cult-project
+
+=end
+
+
 SONG_URLS = [
     "http://dariusroberts.com/songs/Istanbul%20(Not%20Constantinople).mp3",
     "http://dariusroberts.com/songs/Birdhouse%20In%20Your%20Soul.mp3",
@@ -20,7 +40,7 @@ post '/songs' do
       
       xml.Gather(:action => '/play', :numDigits => 1, :method=>'GET') do        
         # list options        
-        xml.Say ' You have dialed Dial-A-Song! You win! A song! Listen carefully and press 9 at any time to repeat this menu.', :voice=>'woman'
+        xml.Say ' You have dialed Dial-A-Song! You win! A song! Listen carefully and press 9 at any time to repeat this menu.'
         xml.Say ' Press 1 to play "Istanbul (Not Constantinople)"'
         xml.Say ' 2 for "Birdhouse in your soul"'
         xml.Say ' 3 for "I\'m Impressed"'
@@ -36,7 +56,7 @@ post '/songs' do
 end
 
 get '/play' do
-  # PSYCH repeat songs menu
+  # PSYCH. repeat songs menu
   case song_number = params['Digits'].to_i
   when 1..5
     # find and play a song
@@ -45,10 +65,8 @@ get '/play' do
     builder do |xml|
        xml.instruct!
        xml.Response do
-         xml.Gather(:action => '/songs', :numDigits => 1, :method=>'POST') do        
-          xml.comment! "Dial-a-song Twilio tutorial by darius.roberts"
-          xml.Play song_url
-        end
+         xml.comment! "Dial-a-song Twilio tutorial by darius.roberts"
+         xml.Play song_url
        end
      end
     
@@ -60,19 +78,5 @@ get '/play' do
          end
        end     
    end
-end
-
-
-post "/flash" do
-    builder do |xml|
-       xml.instruct!
-       xml.Response do
-         xml.Dial do
-           xml.Conference "hacdc"
-           xml.Dial "3043763707"
-           xml.Dial "2022994405"
-         end
-       end
-     end
 end
 
